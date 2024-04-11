40cd04588319ce3775876769833fc1548d82ea5c
Joaquin Jaure <dw22.jaureguiberrytessio.joaquin@gmail.com>
2024-04-11 17:29:05 -0300

    Actualizado

--- src/main/java/com/mycompany/ejercicio2024/Ejercicio2024.java
+++ src/main/java/com/mycompany/ejercicio2024/Ejercicio2024.java
@@ -18,7 +18,10 @@ public class Ejercicio2024 {
                     + "1 - Ver por grupo de números.\n"
                     + "2 - Ver por número.\n"
                     + "3 - Ver Arreglo.\n"
-                    + "4 - Salir del prg.");
+                    + "4 - Ordenar de menor a mayor.\n"
+                    + "5 - Ordenar de mayor a menor.\n"
+                    + "6 - Salir del prg."
+                    + "\n------------------------------\n");
             opcion = leer.nextInt();
             switch(opcion){
                 case 1:
@@ -26,8 +29,8 @@ public class Ejercicio2024 {
                     sum  = leer.nextInt(); 
         
                     for (int i = 0; i < sum + 1; i++) {
-                        primo = nrsPrimo(i);
-                        Arreglo = MostrarNum(Arreglo, primo, i, Arreglo.length);
+                        primo = NumerosPrimos.VerificarPrimo(i);
+                        Arreglo = NumerosPrimos.MostrarNum(Arreglo, primo, i, Arreglo.length);
                     }
                     break;
                 case 2:
@@ -35,8 +38,8 @@ public class Ejercicio2024 {
                         System.out.println("\nColoque un número para saber si es primo. Sino presione 0 para salir:");
                         sum = leer.nextInt();
                         if (sum > 0) {
-                            primo = nrsPrimo(sum);
-                            Arreglo = MostrarNum(Arreglo, primo, sum, Arreglo.length);
+                            primo = NumerosPrimos.VerificarPrimo(sum);
+                            Arreglo = NumerosPrimos.MostrarNum(Arreglo, primo, sum, Arreglo.length);
                         }else{
                             i++;
                         }
@@ -44,64 +47,19 @@ public class Ejercicio2024 {
                     break;
                 case 3:
                     System.out.println("\nLos números primos:");
-                    MostrarArreglo(Arreglo);
+                    NumerosPrimos.MostrarArreglo(Arreglo);
                     break;
                 case 4:
+                    Arreglo = NumerosPrimos.menorMayor(Arreglo);
+                    break;
+                case 5:
+                    Arreglo = NumerosPrimos.mayorMenor(Arreglo);
+                    break;
+                case 6:
                     System.out.println("Has salido del prg.");
                     j++;
                     break;
             }
         }
-    }
-    
-    public static boolean nrsPrimo(int num){
-        
-        if (num <= 1) {
-            return false;
-        }
-        for (int i = 2; i < num; i++) {
-            if (num%i == 0) {   
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public static int[] MostrarNum(int [] Arreglo, boolean primo, int num, int contador){
-        
-        if (primo == true) {
-                Arreglo = RehacerArreglo(Arreglo, contador, num);
-                System.out.println("El número " + num + " ES PRIMO.");
-            }else{
-                System.out.println("El número " + num + " No es primo.");
-            }
-        return Arreglo;
-    }
-    
-    public static void MostrarArreglo(int[] Arreglo){
-        
-        for (int i = 0; i < Arreglo.length; i++) {
-            System.out.println(Arreglo[i]);
-        }
-    }
-    
-    public static int[] RehacerArreglo(int[] Arreglo, int cont, int num){
-        
-        int [] Arreglob = new int[cont];
-        if (cont > 0) {
-            for (int i = 0; i < Arreglob.length; i++) {
-                Arreglob[i] = Arreglo[i];
-            }
-            Arreglo = new int [cont + 1];
-            for (int i = 0; i < Arreglob.length; i++) {
-                Arreglo[i] = Arreglob[i];
-            }
-            Arreglo[cont] = num;
-            Arreglob = new int[0];
-        }else{
-            Arreglo = new int [cont + 1];
-            Arreglo[cont] = num;
-        }
-        return Arreglo;
     }
 }
