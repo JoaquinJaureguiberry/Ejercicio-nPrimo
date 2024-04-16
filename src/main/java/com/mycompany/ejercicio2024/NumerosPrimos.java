610145d9054aaf7182ffd5e3334055225f279d24
Joaquin Jaure <dw22.jaureguiberrytessio.joaquin@gmail.com>
2024-04-16 14:49:23 -0300

    Actualizado

--- src/main/java/com/mycompany/ejercicio2024/NumerosPrimos.java
+++ src/main/java/com/mycompany/ejercicio2024/NumerosPrimos.java
@@ -23,10 +23,10 @@ public class NumerosPrimos {
         return true;
     }
     
-    public static int[] MostrarNum(int [] Arreglo, boolean primo, int num, int contador){
+    public static int[] MostrarNum(int [] Arreglo, boolean primo, int num){
         
         if (primo == true) {
-                Arreglo = RehacerArreglo(Arreglo, contador, num);
+                Arreglo = VerificarArreglo(Arreglo, num);
                 System.out.println("El número " + num + " ES PRIMO.");
             }else{
                 System.out.println("El número " + num + " No es primo.");
@@ -34,22 +34,33 @@ public class NumerosPrimos {
         return Arreglo;
     }
     
-    public static int[] RehacerArreglo(int[] Arreglo, int cont, int num){
+    public static int[] VerificarArreglo(int[] Arreglo, int num){
         
-        int [] Arreglob = new int[cont];
-        if (cont > 0) {
+        for (int i = 0; i < Arreglo.length; i++) {
+            if (num == Arreglo[i]) {
+                return Arreglo;
+            }
+        }
+        Arreglo = RehacerArreglo(Arreglo, num);
+        return Arreglo;
+    }
+    
+    public static int[] RehacerArreglo(int[] Arreglo, int num){
+        
+        int [] Arreglob = new int[Arreglo.length];
+        if (Arreglo.length > 0) {
             for (int i = 0; i < Arreglob.length; i++) {
                 Arreglob[i] = Arreglo[i];
             }
-            Arreglo = new int [cont + 1];
+            Arreglo = new int [Arreglo.length + 1];
             for (int i = 0; i < Arreglob.length; i++) {
                 Arreglo[i] = Arreglob[i];
             }
-            Arreglo[cont] = num;
+            Arreglo[Arreglo.length - 1] = num;
             Arreglob = new int[0];
         }else{
-            Arreglo = new int [cont + 1];
-            Arreglo[cont] = num;
+            Arreglo = new int [Arreglo.length + 1];
+            Arreglo[Arreglo.length - 1] = num;
         }
         return Arreglo;
     }
