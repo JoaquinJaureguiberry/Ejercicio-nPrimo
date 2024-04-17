b3e3611f61faafe43f74d24d6944cba674dca14c
Joaquin Jaure <dw22.jaureguiberrytessio.joaquin@gmail.com>
2024-04-17 11:41:36 -0300

    Actualizado

--- src/main/java/com/mycompany/ejercicio2024/NumerosPrimos.java
+++ src/main/java/com/mycompany/ejercicio2024/NumerosPrimos.java
@@ -3,10 +3,16 @@ package com.mycompany.ejercicio2024;
 
 public class NumerosPrimos {
     
-    public static void MostrarArreglo(int[] Arreglo){
+    private int[] enteros;
+
+    public NumerosPrimos() {
+        this.enteros = new int[0];
+    }
+    
+    public void MostrarArreglo(){
         
-        for (int i = 0; i < Arreglo.length; i++) {
-            System.out.println(Arreglo[i]);
+        for (int i = 0; i < enteros.length; i++) {
+            System.out.println(enteros[i]);
         }
     }
     
@@ -23,81 +29,81 @@ public class NumerosPrimos {
         return true;
     }
     
-    public static int[] MostrarNum(int [] Arreglo, boolean primo, int num){
+    public int[] MostrarNum(boolean primo, int num){
         
         if (primo == true) {
-                Arreglo = VerificarArreglo(Arreglo, num);
+                enteros = VerificarArreglo(num);
                 System.out.println("El número " + num + " ES PRIMO.");
             }else{
                 System.out.println("El número " + num + " No es primo.");
             }
-        return Arreglo;
+        return enteros;
     }
     
-    public static int[] VerificarArreglo(int[] Arreglo, int num){
+    public int[] VerificarArreglo(int num){
         
-        for (int i = 0; i < Arreglo.length; i++) {
-            if (num == Arreglo[i]) {
-                return Arreglo;
+        for (int i = 0; i < enteros.length; i++) {
+            if (num == enteros[i]) {
+                return enteros;
             }
         }
-        Arreglo = RehacerArreglo(Arreglo, num);
-        return Arreglo;
+        enteros = RehacerArreglo(num);
+        return enteros;
     }
     
-    public static int[] RehacerArreglo(int[] Arreglo, int num){
+    public int[] RehacerArreglo(int num){
         
-        int [] Arreglob = new int[Arreglo.length];
-        if (Arreglo.length > 0) {
+        int [] Arreglob = new int[enteros.length];
+        if (enteros.length > 0) {
             for (int i = 0; i < Arreglob.length; i++) {
-                Arreglob[i] = Arreglo[i];
+                Arreglob[i] = enteros[i];
             }
-            Arreglo = new int [Arreglo.length + 1];
+            enteros = new int [enteros.length + 1];
             for (int i = 0; i < Arreglob.length; i++) {
-                Arreglo[i] = Arreglob[i];
+                enteros[i] = Arreglob[i];
             }
-            Arreglo[Arreglo.length - 1] = num;
+            enteros[enteros.length - 1] = num;
             Arreglob = new int[0];
         }else{
-            Arreglo = new int [Arreglo.length + 1];
-            Arreglo[Arreglo.length - 1] = num;
+            enteros = new int [enteros.length + 1];
+            enteros[enteros.length - 1] = num;
         }
-        return Arreglo;
+        return enteros;
     }
     
-    public static int[] menorMayor(int[] Arreglo){
+    public int[] menorMayor(){
         
-        for (int i = 0; i < Arreglo.length; i++) {
-            int sum = Arreglo[i];
+        for (int i = 0; i < enteros.length; i++) {
+            int sum = enteros[i];
             int indice = i;
-            for (int j = i+1; j < Arreglo.length; j++) {
-                if (Arreglo[i] > Arreglo[j]) {
-                    sum = Arreglo[j];
+            for (int j = i+1; j < enteros.length; j++) {
+                if (enteros[i] > enteros[j]) {
+                    sum = enteros[j];
                     indice = j;
                 }
             }
-            int num = Arreglo[i];
-            Arreglo[i] = sum;
-            Arreglo[indice] = num;
+            int num = enteros[i];
+            enteros[i] = sum;
+            enteros[indice] = num;
         }
-        return Arreglo;
+        return enteros;
     }
     
-    public static int[] mayorMenor(int[] Arreglo){
+    public int[] mayorMenor(){
         
-        for (int i = 0; i < Arreglo.length; i++) {
-            int sum = Arreglo[i];
+        for (int i = 0; i < enteros.length; i++) {
+            int sum = enteros[i];
             int indice = i;
-            for (int j = i+1; j < Arreglo.length; j++) {
-                if (Arreglo[i] < Arreglo[j]) {
-                    sum = Arreglo[j];
+            for (int j = i+1; j < enteros.length; j++) {
+                if (enteros[i] < enteros[j]) {
+                    sum = enteros[j];
                     indice = j;
                 }   
             }
-            int num = Arreglo[i];
-            Arreglo[i] = sum;
-            Arreglo[indice] = num;
+            int num = enteros[i];
+            enteros[i] = sum;
+            enteros[indice] = num;
         }
-        return Arreglo;
+        return enteros;
     }
 }
